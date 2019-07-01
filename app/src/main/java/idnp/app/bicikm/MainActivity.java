0e3f4d074473da42172f8a4b8e3bffb40a46d4dc
Mauricio <mauriciomaldonadop@gmai.com>
2019-07-01 16:02:07 -0500

    adding main menu

--- app/src/main/java/idnp/app/bicikm/MainActivity.java
+++ app/src/main/java/idnp/app/bicikm/MainActivity.java
@@ -1,14 +1,26 @@
 package idnp.app.bicikm;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button btnAceptar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAceptar = findViewById(R.id.idbtn1);
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent((getBaseContext()), MenuMainActivity.class));
+            }
+        });
     }
 }
