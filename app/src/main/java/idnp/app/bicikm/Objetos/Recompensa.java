849d463469ff4c9a5a35b92490a755f95e705487
Mauricio <mauriciomaldonadop@gmai.com>
2019-07-11 14:07:46 -0500

    Adding DAO Recompensa

---
+++ app/src/main/java/idnp/app/bicikm/Objetos/Recompensa.java
@@ -0,0 +1,49 @@
+package idnp.app.bicikm.Objetos;
+
+public class Recompensa {
+    private String titulo;
+    private String empresa;
+    private String detalle;
+    private Double costo;
+    private String foto;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public Double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(Double costo) {
+        this.costo = costo;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
