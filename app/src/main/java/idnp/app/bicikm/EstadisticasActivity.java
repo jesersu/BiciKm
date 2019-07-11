a12a8f6e534ada702ede229204014e6c9c01d840
Mauricio <mauriciomaldonadop@gmai.com>
2019-07-11 12:04:44 -0500

    integrations listview to show historial

--- app/src/main/java/idnp/app/bicikm/EstadisticasActivity.java
+++ app/src/main/java/idnp/app/bicikm/EstadisticasActivity.java
@@ -1,6 +1,7 @@
 package idnp.app.bicikm;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -10,19 +11,29 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import idnp.app.bicikm.Objetos.Recorrido;
+
 public class EstadisticasActivity extends AppCompatActivity {
-    String[] array = new String[] {
-            "Elemento 1"
-            ,"Elemento 2"
-            ,"Elemento 3"
-            ,"Elemento 4"
-            ,"Elemento 5"
-            ,"Elemento 6"
-    };
+    ListView listaDet;
+    List<Recorrido> listaRecorridos = new ArrayList<>();
+    String stringId = "salazarmariot@gmail.com";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,9 +63,71 @@ public class EstadisticasActivity extends AppCompatActivity {
                 return false;
             }
         });
-        // ListView
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.listview_row, array);
-        ListView mlistView = findViewById(R.id.list);
-        mlistView.setAdapter(adapter);
+        // Inicializar listView de Estadisticas
+        listaDet = findViewById(R.id.list);
+        MiTask3 task = new MiTask3();
+        String url = "https://bicikm.000webhostapp.com/buscarRecorridoById.php?id=" + stringId;
+
+        task.execute(url);
+    }
+    class MiTask3 extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+        @Override
+        protected String doInBackground(String... strings) {
+            String result = listar(strings[0]);
+            return result;
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                JSONArray datos = new JSONArray(s);
+                for (int i = 0; i < datos.length(); i++) {
+                    JSONObject jsonObject = datos.getJSONObject(i);
+                    Recorrido recorrido = new Recorrido();
+                    recorrido.setUsuario(jsonObject.getString("RecorridoUsuario"));
+                    recorrido.setFecha(jsonObject.getString("RecorridoFecha"));
+                    recorrido.setLatitud(jsonObject.getString("RecorridoLatitud"));
+                    recorrido.setLongitud(jsonObject.getString("RecorridoLongitud"));
+                    listaRecorridos.add(recorrido);
+                }
+            } catch (Exception e) {
+            }
+            listaDet.setAdapter(new EstadisticasAdaptador(listaRecorridos, getApplicationContext()));
+        }
+    }
+
+    public String listar(String direccion) {
+        URL url = null;
+        String linea = "";
+        int respuesta = 0;
+        StringBuilder result = null;
+
+        try {
+            url = new URL(direccion);
+            HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
+            conexion.setRequestMethod("GET");
+            conexion.setDoOutput(true);
+            respuesta = conexion.getResponseCode();//200, 404
+            result = new StringBuilder();
+
+            if (respuesta == HttpURLConnection.HTTP_OK) {
+                InputStream in = new BufferedInputStream(conexion.getInputStream()); //traemos la rpta
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in)); //leemos la rpta
+
+                while ((linea = reader.readLine()) != null) {
+                    result.append(linea);
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
     }
 }
---
+++ app/src/main/java/idnp/app/bicikm/EstadisticasAdaptador.java
@@ -0,0 +1,55 @@
+package idnp.app.bicikm;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import idnp.app.bicikm.Objetos.Recorrido;
+
+public class EstadisticasAdaptador extends BaseAdapter {
+    List<Recorrido> recorridoList = new ArrayList<>();
+    LayoutInflater layoutInflater;
+    Context context;
+
+    public EstadisticasAdaptador (List<Recorrido> recorridoList, Context context) {
+        this.recorridoList = recorridoList;
+        this.context = context;
+        layoutInflater=(LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+        final View vista = layoutInflater.inflate(R.layout.listview_row, null);
+        TextView fecha = (TextView) vista.findViewById(R.id.fecha);
+        TextView latitud = (TextView) vista.findViewById(R.id.latitud);
+        TextView longitud = (TextView) vista.findViewById(R.id.longitud);
+
+        Recorrido recor =getItem(i);
+        //Usuario transnew = getItem(i);
+        fecha.setText(recor.getFecha());
+        latitud.setText(recor.getLatitud());
+        longitud.setText(recor.getLongitud());
+        return vista;
+    }
+
+    @Override
+    public int getCount() {
+        return recorridoList.size();
+    }
+
+    @Override
+    public Recorrido getItem(int position) {
+        return recorridoList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+}
