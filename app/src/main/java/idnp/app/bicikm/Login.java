a00d77aa38b849a9cbba5ed2160592bb3db88e10
clowthsnow <salazarmariot@gmail.com>
2019-07-11 11:55:59 -0500

    DAO Services Login

--- app/src/main/java/idnp/app/bicikm/Login.java
+++ app/src/main/java/idnp/app/bicikm/Login.java
@@ -2,13 +2,24 @@ package idnp.app.bicikm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    private TextView titulo;
+
+    private Typeface estilo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        String fuente="fuentes/title.otf";
+        this.estilo=Typeface.createFromAsset(getAssets(),fuente);
+        titulo=(TextView) findViewById(R.id.bici);
+        titulo.setTypeface(estilo);
+
     }
 }
